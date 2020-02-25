acfb9e58dd11d5d5a20b6e16a7467493dd94cb77
ase1395 <ase1395@MNLIC02DSK369.maxlife.max.com>
2020-02-25 10:57:39 +0530

    Switchmodifiedprogram

--- TestProject03/src/SwitchFile.java
+++ TestProject03/src/SwitchFile.java
@@ -3,7 +3,18 @@ public class SwitchFile
 {
 	public static void main(String [] args)
 	{
-		
+		int a=2;
+		switch(a)
+		{
+		case 1: System.out.println("Monday");
+		     break;
+		case 2: System.out.println("Tuesday");
+	     break;
+		case 3: System.out.println("Wednesday");
+	     break;
+		case 4: System.out.println("Sunday");
+	     break;
+		}
 	}
 
 }
